13d07503e143ebb8f43f337c329e8d0a73ec4da6
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-01-27 01:56:43 +0300

    initial setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function Home(){
+    return(
+        <Box>
+            <Heading>Link sharing app</Heading>
+        </Box>
+    )
+}
