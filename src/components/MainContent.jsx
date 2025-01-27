b688f6ca34fff65c6ae7ead2c4915f406585bc1c
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-01-27 11:25:35 +0300

    added add link modal form

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,8 +1,9 @@
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, FormControl, FormLabel, Heading, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
 import { FaPlus } from "react-icons/fa6";
 import LinkCard from "./LinkCard";
 
 export default function MainContent() {
+  const {isOpen, onOpen, onClose}=useDisclosure();
   return (
     <Box
       boxShadow={"lg"}
@@ -28,9 +29,32 @@ export default function MainContent() {
           variant={"outline"}
           colorScheme="purple"
           w={"100%"}
+          onClick={onOpen}
         >
           Add new link
         </Button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay/>
+          <ModalContent>
+            <ModalHeader fontSize={"md"}>Add new link</ModalHeader>
+            <ModalCloseButton/>
+            <ModalBody>
+              <form>
+                <FormControl my={2}>
+                  <FormLabel fontSize={"sm"} color={"gray"}>Platform</FormLabel>
+                  <Input type="text" fontSize={"sm"} />                  
+                </FormControl>
+                <FormControl>
+                  <FormLabel fontSize={"sm"} color={"gray"}>Link</FormLabel>
+                  <Input type="text" fontSize={"sm"} />                  
+                </FormControl>
+              </form>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="purple" type="submit">Add</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </Box>
       <Box>
         <LinkCard />
