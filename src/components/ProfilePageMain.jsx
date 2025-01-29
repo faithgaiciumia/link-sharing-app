9bea2d9369408faace46be3ed4a964941056602e
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-01-29 23:01:22 +0300

    added profile state to profile page

--- src/components/ProfilePageMain.jsx
+++ src/components/ProfilePageMain.jsx
@@ -9,8 +9,20 @@ import {
   Input,
   Text,
 } from "@chakra-ui/react";
+import useLinkStore from "../store/useLinkStore";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 
 export default function ProfilePageMain() {
+  const { profile, updateProfile } = useLinkStore();
+  const { register, handleSubmit, reset } = useForm();
+  useEffect(() => {
+    reset(profile);
+  }, [profile, reset]);
+  const onSubmit = (data) => {
+    updateProfile(data);
+    reset();
+  };
   return (
     <Box
       boxShadow={"lg"}
@@ -38,27 +50,63 @@ export default function ProfilePageMain() {
         p={2}
       >
         <Text fontSize={"sm"}>Profile picture</Text>
-        <Image src="" w={"200px"} h={"200px"} alt="profile" />
+        <Image
+          src={profile.avatar || "https://placehold.co/200"}
+          w={"200px"}
+          h={"200px"}
+          alt="profile"
+        />
         <Text fontSize={"sm"}>
           Image must be below 1024px*1024px. Use PNG, JPG, or RMP format.
         </Text>
       </Flex>
       <Box my={2} background={"gray.100"} borderRadius={"md"} p={2}>
-        <FormControl isRequired display={"flex"} my={2}>
-          <FormLabel w={"40%"}>First Name</FormLabel>
-          <Input placeholder="Ben" w={"60%"} borderColor={"gray.700"} />
-        </FormControl>
-        <FormControl isRequired display={"flex"} my={2}>
-          <FormLabel w={"40%"}>Last Name</FormLabel>
-          <Input placeholder="Ben" w={"60%"} borderColor={"gray.700"} />
-        </FormControl>
-        <FormControl display={"flex"} my={2}>
-          <FormLabel w={"40%"}>Email</FormLabel>
-          <Input placeholder="Ben" w={"60%"} borderColor={"gray.700"} />
-        </FormControl>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl isRequired display={"flex"} my={2}>
+            <FormLabel w={"40%"}>First Name</FormLabel>
+            <Input
+              placeholder="Ben"
+              w={"60%"}
+              borderColor={"gray.700"}
+              {...register("firstName", { required: true })}
+            />
+          </FormControl>
+          <FormControl isRequired display={"flex"} my={2}>
+            <FormLabel w={"40%"}>Last Name</FormLabel>
+            <Input
+              placeholder="Ben"
+              w={"60%"}
+              borderColor={"gray.700"}
+              {...register("lastName", { required: true })}
+            />
+          </FormControl>
+          <FormControl isRequired display={"flex"} my={2}>
+            <FormLabel w={"40%"}>Bio</FormLabel>
+            <Input
+              placeholder="Ben"
+              w={"60%"}
+              borderColor={"gray.700"}
+              {...register("bio", { required: true })}
+            />
+          </FormControl>
+          <FormControl display={"flex"} my={2}>
+            <FormLabel w={"40%"}>Email</FormLabel>
+            <Input
+              placeholder="Ben"
+              w={"60%"}
+              borderColor={"gray.700"}
+              {...register("email", { required: true })}
+            />
+          </FormControl>
+        </form>
       </Box>
       <Flex justifyContent={"end"}>
-        <Button colorScheme="purple" size={"md"}>
+        <Button
+          colorScheme="purple"
+          size={"md"}
+          type="submit"
+          onClick={handleSubmit(onSubmit)}
+        >
           Save
         </Button>
       </Flex>
