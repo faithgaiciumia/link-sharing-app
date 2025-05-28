19dcd891da0262937903b717e8a28437162e9495
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-05-28 22:13:40 +0300

    redirect logged in users past landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,14 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Box } from "@chakra-ui/react";
 import LandingNav from "../components/Landing/LandingNav";
 import Hero from "../components/Landing/Hero";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { fetchSession } from "../data/fetchSession";
 
-export default function Landing(){
-    return(
-        <Box p={4}>
-        <LandingNav/>
-        <Box my={4}>
-            <Hero/>
-        </Box>
-        </Box>
-    )
+export default function Landing() {
+  //check for already logged in users
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const session = await fetchSession();
+      if (session?.user?.email) {
+        navigate("/home");
+      }
+    };
+    loadUser();
+  }, []);
+  return (
+    <Box p={4}>
+      <LandingNav />
+      <Box my={4}>
+        <Hero />
+      </Box>
+    </Box>
+  );
 }
