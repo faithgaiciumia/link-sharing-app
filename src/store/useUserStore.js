5736c97d4069d248c9a0e76cccee480444410327
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-06-01 22:13:51 +0300

    updated user store

--- src/store/useUserStore.js
+++ src/store/useUserStore.js
@@ -1,11 +1,21 @@
+// store/useUserStore.js
 import { create } from "zustand";
 import { fetchCurrentUser } from "../data/fetchCurrentUser";
 
 const useUserStore = create((set) => ({
   user: null,
-  fetchAndSetUser: async () => {
-    const user = await fetchCurrentUser();
-    set({ user });
+  loading: false,
+  fetchUser: async () => {
+    set({ loading: true });
+    try {
+      const user = await fetchCurrentUser();
+      set({ user });
+    } catch (error) {
+      console.error("Failed to fetch user:", error);
+    } finally {
+      set({ loading: false });
+    }
   },
 }));
+
 export default useUserStore;
