9e0524dbd87e11354eaa68bf3d59d7689ca2a209
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-06-05 00:03:01 +0300

    logout button refined

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -1,22 +1,42 @@
+import { Button, useToast } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { logoutUser } from "../data/logoutUser";
 
 const LogoutButton = () => {
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toast = useToast();
 
   const handleLogout = async () => {
+    setIsLoading(true);
     const success = await logoutUser();
+    setIsLoading(false);
     if (success) {
       navigate("/");
     } else {
-      alert("Logout failed");
+      toast({
+        status: "error",
+        description: "Please try again",
+        title: "Logout Failed",
+        duration: 3000,
+      });
     }
   };
 
   return (
-    <button onClick={handleLogout}>
+    <Button
+      colorScheme="red"
+      variant="solid"
+      size="sm"
+      w={"100%"}
+      onClick={handleLogout}
+      isLoading={isLoading}
+      loadingText="Logging out..."
+    >
       Logout
-    </button>
+    </Button>
   );
 };
 
